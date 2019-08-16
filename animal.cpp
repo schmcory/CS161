af9874e70c6001985284cd2581a6403c445c0acc
schmcory <36436049+schmcory@users.noreply.github.com>
2019-08-16 18:22:25 -0500

    Create animal.cpp
    This simple program prints your favorite animal to the console.

---
+++ animal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+// a simple example program
+int main()
+{
+    std::string faveAnimal;
+    std::cout << "Please enter your favorite animal." << std::endl;
+    std::cin >> faveAnimal;
+    std::cout << "Your favorite animal is the " << faveAnimal;
+    std::cout << "." << std::endl;    
+    
+    return 0;
+}
