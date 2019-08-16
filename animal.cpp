212915b399f0483b7a1e6736b6f16c94abfd1567
schmcory <36436049+schmcory@users.noreply.github.com>
2019-08-16 18:29:41 -0500

    Update animal.cpp

--- animal.cpp
+++ animal.cpp
@@ -1,14 +1,23 @@
-#include <iostream>
+#include iostream
 #include <string>
 
-// a simple example program
-int main()
-{
-    std::string faveAnimal;
-    std::cout << "Please enter your favorite animal." << std::endl;
-    std::cin >> faveAnimal;
-    std::cout << "Your favorite animal is the " << faveAnimal;
-    std::cout << "." << std::endl;    
-    
-    return 0;
+int main () {
+
+		//this variable, string type, holds the value the user prints to the console
+		string faveAnimal;	
+
+		//statement prints a question to the console
+		std::cout << "What is your favorite animal?" << endl;
+
+		//prompt for user to enter a string value to the console
+		std::cin >> faveAnimal;
+
+		//statement prints the users favorite animal
+		std::cout << "Your favorite animal is the " << faveAnimal;
+
+		//statements ends the sentence with a period
+		std::cout << "." << endl;
+
+		//terminate the main function
+		return 0;
 }
