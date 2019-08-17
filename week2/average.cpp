27f4bc9eff8dd39fc0487ecf1127ee4be0a93231
schmcory <36436049+schmcory@users.noreply.github.com>
2019-08-17 00:03:40 -0500

    Update average.cpp

--- week2/average.cpp
+++ week2/average.cpp
@@ -3,27 +3,35 @@
 #include <iomanip>
 
 int main() {
-	double number[5];
-	double average;
-	double sum;
+	double number[5];       //array variable holds five numbers entered by the user
+    double sum;             //sum variable holds the sum of the numbers
+	double average;         //average variable holds the average of the 5 numbers
 
 	std::cout << "Please enter five numbers: " << std::endl;
 
+    //for loop adds numbers the the array and adds them up
 	for(int i=0; i < 5; i++) {
 		std::cin >> number[i];
-		sum += number[i];
+        sum += number[i];;
 
+        //input validation
 		if(std::cin.fail()) {
 			std::cout << "Please only enter a number." << std::endl;
 			std::cin.clear();
 			std::cin.ignore();
-			std::cin >> number[i];
+			i -= 1;
 		}
 	}
 
+    //average is the sum of all of the numbers divided by 5
 	average = sum/5;
 
+    //for testing purposes
+    //std::cout << "The sum of these numbers are: " << sum << std::endl;
+
+    //final print statement
 	std::cout << "The average of those numbers is: " << average << std::endl;
 
+    //terminate program
 	return 0;
 }
