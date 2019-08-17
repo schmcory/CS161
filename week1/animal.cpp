ca996a907c3bcb8b63d70daad7cac939d0079e32
schmcory <36436049+schmcory@users.noreply.github.com>
2019-08-16 21:50:31 -0500

    Update animal.cpp

--- week1/animal.cpp
+++ week1/animal.cpp
@@ -1,16 +1,25 @@
-#include iostream
+#include <iostream>
 #include <string>
 
 int main () {
 
 		//this variable, string type, holds the value the user prints to the console
-		string faveAnimal;	
+		std::string faveAnimal;
 
 		//statement prints a question to the console
 		std::cout << "What is your favorite animal?" << std::endl;
 
 		//prompt for user to enter a string value to the console
 		std::cin >> faveAnimal;
+
+		for(int i = 0; i < faveAnimal.size(); i++) {
+			if(!(isalpha(faveAnimal[i]))) {
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout << "Please enter only letters." << std::endl;
+				std::cin >> faveAnimal;
+			}
+		}
 
 		//statement prints the users favorite animal
 		std::cout << "Your favorite animal is the " << faveAnimal;
@@ -21,3 +30,4 @@ int main () {
 		//terminate the main function
 		return 0;
 }
+
